7fbf4cc768477cc3c5d29e8fe53d141d5b6b673d
Alex Mohedano Labra <alex@MacBook-Pro-de-Alex.local>
2018-04-19 12:07:40 -0500

    sisisi

--- app/components/tarjeta-unidad.js
+++ app/components/tarjeta-unidad.js
@@ -1,6 +1,11 @@
 import Component from '@ember/component';
+import {computed} from "@ember/object";
 
 export default Component.extend({
+  admin1Real: computed('housing-unit.admin1.content', function(){
+    return this.get('housing-unit.admin1.content')
+  }),
+
   actions:{
     borrarUnidad(unit){
       unit.destroyRecord()
